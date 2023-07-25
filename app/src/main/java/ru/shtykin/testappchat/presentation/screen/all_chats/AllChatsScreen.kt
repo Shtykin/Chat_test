fdf3a0dcf162295d7e60de9ca4a767e8dc19f84c
Shtykin <shtykin@mail.ru>
2023-07-25 22:21:36 +0800

    начал скидывать боковое меню

--- app/src/main/java/ru/shtykin/testappchat/presentation/screen/all_chats/AllChatsScreen.kt
+++ app/src/main/java/ru/shtykin/testappchat/presentation/screen/all_chats/AllChatsScreen.kt
@@ -1,16 +1,43 @@
 package ru.shtykin.testappchat.presentation.screen.all_chats
 
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LinkOff
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.outlined.Menu
+import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberDrawerState
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
+import ru.shtykin.testappchat.R
 import ru.shtykin.testappchat.presentation.state.ScreenState
 
 
@@ -20,27 +47,62 @@ fun AllChatsScreen(
     uiState: ScreenState,
     onProfileClick: (() -> Unit)?,
 ) {
+    val scope = rememberCoroutineScope()
     val drawerState = rememberDrawerState(DrawerValue.Closed)
 
     ModalNavigationDrawer(
         drawerState = drawerState,
         drawerContent = {
             ModalDrawerSheet {
+                Column() {
+                    Text(text = "123")
+                }
 
             }
         },
         content = {
+            val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+            Scaffold(
+                modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+                topBar = {
+                    TopAppBar(
+                        title = { Text(text = stringResource(R.string.app_name)) },
+                        navigationIcon = {
+                            IconButton(onClick = { scope.launch { drawerState.open() } }) {
+                                Icon(
+                                    imageVector = Icons.Outlined.Menu,
+                                    contentDescription = null
+                                )
+                            }
+                        },
+                        actions = {
+                            IconButton(onClick = {  }) {
+                                Icon(
+                                    imageVector = Icons.Outlined.Search,
+                                    contentDescription = null,
+                                    tint = MaterialTheme.colorScheme.background
+                                )
+                            }
+                        },
+                        colors = TopAppBarDefaults.smallTopAppBarColors(
+                            containerColor = MaterialTheme.colorScheme.primary,
+                            titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                            navigationIconContentColor = MaterialTheme.colorScheme.onPrimary
+                        ),
+                        scrollBehavior = scrollBehavior
+                    )
+                },
+            ) { paddingValues ->
+                LazyColumn(contentPadding = paddingValues) {
+                    item { Button(onClick = { onProfileClick?.invoke() }) {
+                        Text(text = "Профиль")
+                    } }
+                }
 
+            }
         }
     )
 
-    Scaffold() { paddingValues ->
-        LazyColumn(contentPadding = paddingValues) {
-            item { Button(onClick = { onProfileClick?.invoke() }) {
-                Text(text = "Профиль")
-            } }
-        }
 
-    }
 
 }
--- app/src/main/java/ru/shtykin/testappchat/presentation/screen/edit_profile/EditProfileScreen.kt
+++ app/src/main/java/ru/shtykin/testappchat/presentation/screen/edit_profile/EditProfileScreen.kt
@@ -1,7 +1,6 @@
 package ru.shtykin.testappchat.presentation.screen.edit_profile
 
 import android.graphics.Bitmap
-import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -38,7 +37,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.room.util.appendPlaceholders
 import coil.compose.AsyncImage
 import coil.compose.rememberAsyncImagePainter
 import com.google.accompanist.swiperefresh.SwipeRefresh
